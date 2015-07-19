09cafd689d4a04c63dd6ed7b839bc334fdf67e81
pritz2 <pritzluke@gmail.com>
2015-07-19 01:34:22 -0500

    Pieces can now fall down the screen

--- Tketris.py
+++ Tketris.py
@@ -2,7 +2,7 @@ from tkinter import Tk, Canvas
 
 class Tetris():
     WIDTH = 500
-    HEIGHT = 650
+    HEIGHT = 900
     
     def init(self):
         self.root = Tk()
@@ -14,14 +14,26 @@ class Tetris():
                 height=Tetris.HEIGHT)
         self.canvas.pack()
 
+        self.current_piece = Piece(self.canvas)
+
         self.time_interval = 500
         self.timer()
         
         self.root.mainloop()
 
     def timer(self):
-        print("Hello")
         self.root.after(self.time_interval, self.timer)
+        self.current_piece.fall()
+
+class Piece():
+    BOX_SIZE = 50
+    
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.box = self.canvas.create_rectangle(100, 0, Piece.BOX_SIZE, Piece.BOX_SIZE, fill="blue")
+
+    def fall(self):
+        self.canvas.move(self.box, 0, Piece.BOX_SIZE)
 
 if __name__ == "__main__":
     game = Tetris()
