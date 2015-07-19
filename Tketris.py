3ebd43355fea2c4fb55109cc03ed870b5a3ca9cd
pritz2 <pritzluke@gmail.com>
2015-07-19 00:41:50 -0500

    Added a basic timer function

--- Tketris.py
+++ Tketris.py
@@ -13,8 +13,15 @@ class Tetris():
                 width=Tetris.WIDTH, 
                 height=Tetris.HEIGHT)
         self.canvas.pack()
+
+        self.time_interval = 500
+        self.timer()
         
         self.root.mainloop()
+
+    def timer(self):
+        print("Hello")
+        self.root.after(self.time_interval, self.timer)
 
 if __name__ == "__main__":
     game = Tetris()
